7260b76cb2a3129c0cd3e892c20b1c37ab09cd99
Iryna-Aleksandrova <ira_aleksandrova@ukr.net>
2025-04-13 10:44:21 +0100

    hw 4

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,21 +7,43 @@ import {
   patchContactsController,
   deleteContactsController,
 } from '../controllers/contacts.js';
-
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../utils/validateBody.js';
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from '../validations/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/', ctrlWrapper(getContactsController));
 
-contactsRouter.get('/:contactId', ctrlWrapper(getContactsByIdController));
+contactsRouter.get(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(getContactsByIdController),
+);
 
-contactsRouter.post('/', ctrlWrapper(addContactsController));
+contactsRouter.post(
+  '/',
+  validateBody(contactAddSchema),
+  ctrlWrapper(addContactsController),
+);
 
-// contactsRouter.put('/:contactId', ctrlWrapper(upsertContactsController));
+// contactsRouter.put('/:contactId', isValidId, validateBody(contactAddSchema), ctrlWrapper(upsertContactsController));
 
-contactsRouter.patch('/:contactId', ctrlWrapper(patchContactsController));
+contactsRouter.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(contactUpdateSchema),
+  ctrlWrapper(patchContactsController),
+);
 
-contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactsController));
+contactsRouter.delete(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactsController),
+);
 
 export default contactsRouter;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,50 @@
 import ContactsCollection from '../db/models/Contact.js';
+import { calcPaginationData } from '../utils/calcPaginationData.js';
+import { sortList } from '../constants/index.js';
 
-export const getContacts = async () => {
-  try {
-    return await ContactsCollection.find();
-  } catch (error) {
-    throw new Error(error);
+export const getContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortBy = '_id',
+  sortOrder = sortList[0],
+  filters = {},
+}) => {
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+
+  if (filters.contactType) {
+    contactsQuery.where('contactType').equals(filters.contactType);
   }
+
+  if (filters.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filters.isFavourite);
+  }
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(perPage)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const countQuery = ContactsCollection.find();
+
+  if (filters.contactType) {
+    countQuery.where('contactType').equals(filters.contactType);
+  }
+
+  if (filters.isFavourite !== undefined) {
+    countQuery.where('isFavourite').equals(filters.isFavourite);
+  }
+
+  const totalItems = await countQuery.countDocuments();
+
+  const paginationData = calcPaginationData({ page, perPage, totalItems });
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactsById = async (id) => {
